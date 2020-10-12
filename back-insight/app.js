e3bef5d94d18b8c841f78f8d7dd75b0a5125cd51
Tharushan <tharushan.saseeskumar@mydposolution.com>
2020-10-12 22:41:03 +0200

    Add: bootup time performance score front + back

--- back-insight/app.js
+++ back-insight/app.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 const express = require('express');
 
 const PerformanceScore = require('./models/PerformanceScore');
+const BootupTimeScore = require('./models/BootupTimeScore');
 
 const app = express();
 require('dotenv').config();
@@ -17,6 +18,19 @@ app.get('/performance-score', async (req, res) => {
       .orderBy('date_performance', 'desc')
       .limit(10);
     res.json(performances);
+  } catch (error) {
+    console.error(error.message);
+    res.send(error);
+  }
+});
+
+app.get('/bootuptime-score', async (req, res) => {
+  try {
+    const bootupTimes = await BootupTimeScore.query()
+      .orderBy('date_bootup', 'desc')
+      .limit(10);
+      console.log(bootupTimes)
+    res.json(bootupTimes);
   } catch (error) {
     console.error(error.message);
     res.send(error);
